fe1166f29489ae6c94878c8d9b36bd6ec357c17f
JuliaSese <julia.sese0@gmail.com>
2017-11-09 15:12:59 +0100

    mise à jour crud jouets

--- src/AppBundle/Form/JouetsType.php
+++ src/AppBundle/Form/JouetsType.php
@@ -13,7 +13,7 @@ class JouetsType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nom');
+        $builder->add('nom')->add('vignette');
     }
     
     /**
